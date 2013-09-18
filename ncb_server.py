c397ddcbf53dfe54974ae8cfed3a46a31b017e59
Nathan Jordan <natedagreat27274@gmail.com>
2013-09-17 21:58:56 -0700

    flask work

---
+++ ncb_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_from_directory
+
+# Create new application
+app = Flask(__name__)
+
+# Debugging is okay for now
+app.debug = True
+
+# Serves the main application
+@app.route('/')
+def mainPage():
+    # Test for templated rendering
+    testVar = 'This is a test'
+    return render_template('index.html', test = testVar)
+
+# Serves static resources like CSS, js, images, etc.
+@app.route('/assets/<path:resource>')
+def serveStaticResource(resource):
+    return send_from_directory('static/assets/', resource)
+
+# Run the server if this file is run directly
+if __name__ == '__main__':
+    app.run()
