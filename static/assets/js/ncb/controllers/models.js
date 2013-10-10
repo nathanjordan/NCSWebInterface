2510803603859bce6f734c2533febba4e091bf9e
Nathan Jordan <natedagreat27274@gmail.com>
2013-10-10 15:29:27 -0700

    yay scope injection

--- static/assets/js/ncb/controllers/models.js
+++ static/assets/js/ncb/controllers/models.js
@@ -2,6 +2,6 @@ define(['angular'], function(angular) {
 
     // This will be changed to be more specific to a particular task
     function ModelBuilderController($scope, $routeParams) {
-
+        $scope.x = 'test';
     }
 });
